8c7f696d26574696b44cd1291a4dba6533467724
BestGak <pro100gall@yandex.ru>
2021-09-01 23:40:00 +0300

    fix pug

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const popUpClose = document.querySelector('.popUp__close')
 popUp.addEventListener('click', () =>{
   popUpMenu.classList.add('block')
 })
-
 popUpClose.addEventListener('click' , () =>{
   popUpMenu.classList.remove('block')
 })
